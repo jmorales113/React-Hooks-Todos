003e67a1a29717881797d45cbc25027bfc62bcac
jmorales113 <jmorales113@gmail.com>
2020-05-20 14:46:06 -0700

    Able to add new todo

--- react-hooks-todos/src/App.js
+++ react-hooks-todos/src/App.js
@@ -10,13 +10,20 @@ function TodoForm({addTodo}) {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if(!value) return addTodo(value)
+    if(!value) 
+    return 
+    addTodo(value) 
     setValue("")
   }
 
   return (
-    <form onSumbit={handleSubmit}>
-    <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)} />
+    <form onSubmit={handleSubmit}>
+    <input 
+    type="text" 
+    className="input"
+    placeholder="Add Todo..." 
+    value={value} 
+    onChange={e => setValue(e.target.value)} />
     </form>
   )
 }
@@ -37,12 +44,19 @@ function App() {
     }
   ])
 
+  const addTodo = (text) => {
+    const newTodos = [...todos, {text} ]
+    setTodos(newTodos)
+
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
       {todos.map((todo, index) => (
         <Todo key={index} todo={todo} index={index} />
       ))}
+      <TodoForm addTodo={addTodo} />
       </div>
     </div>
   )
